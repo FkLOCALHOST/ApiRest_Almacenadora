132e3b29a03cd531ffd658ec04d09f8d5001ad6e
CarlosMorales-2021365 <moralesc21365@gmail.com>
2025-05-01 14:37:39 -0600

    fix(Clientes): se arreglaron los validadores de clientes

--- src/middlewares/clientes-validators.js
+++ src/middlewares/clientes-validators.js
@@ -8,8 +8,8 @@ import { hasRoles } from "./validate-roles.js";
 export const agregarClienteValidador = [
     validateJWT,
     hasRoles("ADMIN_ROLE"),
-    body('nombre').notEmpty().withMessage('El nombre es requerido').isLength({ max: 25 }).withMessage("El nombre no puede superar los 25 caracteres"),,
-    body('apellido').notEmpty().withMessage('El apellido es requerido').isLength({ max: 25 }).withMessage("El apellido no puede superar los 25 caracteres"),,
+    body('nombre').notEmpty().withMessage('El nombre es requerido').isLength({ max: 25 }).withMessage("El nombre no puede superar los 25 caracteres"),
+    body('apellido').notEmpty().withMessage('El apellido es requerido').isLength({ max: 25 }).withMessage("El apellido no puede superar los 25 caracteres"),
     body("correo").notEmpty().withMessage("El correo es requerido"),
     body('correo').isEmail().withMessage('El correo no es valido'),
     body('correo').custom(correoExistente),
@@ -20,6 +20,7 @@ export const agregarClienteValidador = [
 ];
 
 export const obtenerClientePorIdValidador = [
+    validateJWT,
     param('id').isMongoId().withMessage('El ID no es valido'),
     param('id').custom(clienteExistente),
     validarCampos,
@@ -27,6 +28,7 @@ export const obtenerClientePorIdValidador = [
 ];
 
 export const listarClientesValidador = [
+    validateJWT,
     validarCampos,
     handleErrors
 ];
@@ -45,11 +47,10 @@ export const actualizarClientesValidador = [
     hasRoles("ADMIN_ROLE"),
     param('id').isMongoId().withMessage('El ID no es valido'),
     param('id').custom(clienteExistente),
-    body('nombre').optional().withMessage('El nombre es requerido').isLength({ max: 25 }).withMessage("El nombre no puede superar los 25 caracteres"),,
-    body('apellido').optional().withMessage('El apellido es requerido').isLength({ max: 25 }).withMessage("El apellido no puede superar los 25 caracteres"),,
-    body("correo").optional().withMessage("El correo es requerido"),
-    body('correo').isEmail().withMessage('El correo no es valido'),
-    body('telefono').optional().withMessage('El telefono es requerido').matches(/^\d+$/).withMessage("El Telefono debe contener solo números"),
+    body('nombre').optional().isLength({ max: 25 }).withMessage('El nombre es requerido').withMessage("El nombre no puede superar los 25 caracteres"),
+    body('apellido').optional().isLength({ max: 25 }).withMessage('El apellido es requerido').withMessage("El apellido no puede superar los 25 caracteres"),
+    body("correo").optional().isEmail().withMessage('El correo no es valido'),
+    body('telefono').optional().matches(/^\d+$/).withMessage("El Telefono debe contener solo números"),
     body('telefono').isLength({ min: 8, max: 8}).withMessage('El telefono debe de tener 8 digitos'),
     validarCampos,
     handleErrors
